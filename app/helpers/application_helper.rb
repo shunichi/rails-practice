7352fdf317dea425fb4ad888c2c22d729747fbb4
Shunichi Ikegami <sike.tm@gmail.com>
2015-04-19 17:10:02 +0900

    Add markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def markdown(text)
+    renderer = Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML.new(hard_wrap: true),
+        no_intra_emphasis: true,
+        autolink: true,
+        strikethrough: true,
+        lax_spacing: true,
+        tables: true,
+        fenced_code_blocks: true,
+        space_after_headers: true)
+    renderer.render(sanitize(text)).html_safe
+  end
 end
